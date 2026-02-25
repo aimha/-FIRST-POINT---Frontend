794fd589e0e07d34ec49fa90865dd5830f5cb7b5
Christian Boragine <christian.boragine@gmail.com>
2026-02-25 11:01:39 +0100

    contatti modal refactored

--- src/routes/Contatti/components/ContactDetails.jsx
+++ src/routes/Contatti/components/ContactDetails.jsx
@@ -39,141 +39,143 @@ function ContactDetail(props) {
         </Switch>
       </div>
 
-      <div class={styles.Contact__row}>
-        <div class={styles.Contact__cell}>
-          <span>Azienda</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.azienda || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <input type="text" name="azienda" value={formData().azienda} onInput={handleInputChange} />
-            </Match>
-          </Switch>
+      <div class={styles.Contact__block}>
+        <div class={styles.Contact__row}>
+          <div class={styles.Contact__cell}>
+            <span>Azienda</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.azienda || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <input type="text" name="azienda" value={formData().azienda} onInput={handleInputChange} />
+              </Match>
+            </Switch>
+          </div>
+
+          <div class={styles.Contact__cell}>
+            <span>Dipartimento</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.dipartimento || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <input type="text" name="dipartimento" value={formData().dipartimento} onInput={handleInputChange} />
+              </Match>
+            </Switch>
+          </div>
+
+          <div class={styles.Contact__cell}>
+            <span>Email</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.email || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <input type="text" name="email" value={formData().email} onInput={handleInputChange} />
+              </Match>
+            </Switch>
+          </div>
         </div>
 
-        <div class={styles.Contact__cell}>
-          <span>Dipartimento</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.dipartimento || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <input type="text" name="dipartimento" value={formData().dipartimento} onInput={handleInputChange} />
-            </Match>
-          </Switch>
+        <div class={styles.Contact__row}>
+          <div class={styles.Contact__cell}>
+            <span>Cellulare</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.cellulare || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <input type="text" name="cellulare" value={formData().cellulare} onInput={handleInputChange} />
+              </Match>
+            </Switch>
+          </div>
+
+          <div class={styles.Contact__cell}>
+            <span>Cellulare 2</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.cellulare2 || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <input type="text" name="cellulare2" value={formData().cellulare2} onInput={handleInputChange} />
+              </Match>
+            </Switch>
+          </div>
+
+          <div class={styles.Contact__cell}>
+            <span>Casa</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.case || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <input type="text" name="casa" value={formData().casa} onInput={handleInputChange} />
+              </Match>
+            </Switch>
+          </div>
         </div>
 
-        <div class={styles.Contact__cell}>
-          <span>Email</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.email || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <input type="text" name="email" value={formData().email} onInput={handleInputChange} />
-            </Match>
-          </Switch>
-        </div>
-      </div>
+        <div class={styles.Contact__row}>
+          <div class={styles.Contact__cell}>
+            <span>Ufficio</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.ufficio || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <input type="text" name="ufficio" value={formData().ufficio} onInput={handleInputChange} />
+              </Match>
+            </Switch>
+          </div>
 
-      <div class={styles.Contact__row}>
-        <div class={styles.Contact__cell}>
-          <span>Cellulare</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.cellulare || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <input type="text" name="cellulare" value={formData().cellulare} onInput={handleInputChange} />
-            </Match>
-          </Switch>
-        </div>
+          <div class={styles.Contact__cell}>
+            <span>Ufficio 2</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.cellulare2 || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <input type="text" name="ufficio2" value={formData().ufficio2} onInput={handleInputChange} />
+              </Match>
+            </Switch>
+          </div>
 
-        <div class={styles.Contact__cell}>
-          <span>Cellulare 2</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.cellulare2 || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <input type="text" name="cellulare2" value={formData().cellulare2} onInput={handleInputChange} />
-            </Match>
-          </Switch>
-        </div>
-
-        <div class={styles.Contact__cell}>
-          <span>Casa</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.case || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <input type="text" name="casa" value={formData().casa} onInput={handleInputChange} />
-            </Match>
-          </Switch>
-        </div>
-      </div>
-
-      <div class={styles.Contact__row}>
-        <div class={styles.Contact__cell}>
-          <span>Ufficio</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.ufficio || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <input type="text" name="ufficio" value={formData().ufficio} onInput={handleInputChange} />
-            </Match>
-          </Switch>
-        </div>
-
-        <div class={styles.Contact__cell}>
-          <span>Ufficio 2</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.cellulare2 || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <input type="text" name="ufficio2" value={formData().ufficio2} onInput={handleInputChange} />
-            </Match>
-          </Switch>
-        </div>
-
-        <div class={styles.Contact__cell}>
-          <span>Fax</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.fax || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <input type="text" name="fax" value={formData().fax} onInput={handleInputChange} />
-            </Match>
-          </Switch>
-        </div>
-      </div>
-
-
-      <div class={`${styles.Contact__row} ${styles['Contact__row--last']}`}>
-        <div class={styles.Contact__cell}>
-          <span>Note</span>
-          <Switch>
-            <Match when={!isEditing()}>
-              <p>{props.data.altro || "---"}</p>
-            </Match>
-            <Match when={isEditing()}>
-              <textarea
-                name="altro"
-                value={formData().altro}
-                onInput={handleInputChange}
-                rows="3"
-                class={styles.TextareaCustom} // Usa una classe specifica per gestire il resize e il font
-              />
-            </Match>
-          </Switch>
+          <div class={styles.Contact__cell}>
+            <span>Fax</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.fax || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <input type="text" name="fax" value={formData().fax} onInput={handleInputChange} />
+              </Match>
+            </Switch>
+          </div>
         </div>
       </div>
 
+      <div class={styles.Contact__block}>
+        <div class={`${styles.Contact__row} ${styles.Contact__notes}`}>
+          <div class={styles.Contact__cell}>
+            <span>Note</span>
+            <Switch>
+              <Match when={!isEditing()}>
+                <p>{props.data.altro || "---"}</p>
+              </Match>
+              <Match when={isEditing()}>
+                <textarea
+                  name="altro"
+                  value={formData().altro}
+                  onInput={handleInputChange}
+                  rows="3"
+                  class={styles.TextareaCustom} // Usa una classe specifica per gestire il resize e il font
+                />
+              </Match>
+            </Switch>
+          </div>
+        </div>
+      </div>
       <div class={styles.ModalActions}>
         <Switch>
           <Match when={!isEditing()}>
